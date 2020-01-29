1bfdc9a87a64d07eb8db7a4fdf6e5b036c385194
MusicDev33 <smccowan33@gmail.com>
2020-01-29 09:00:08 -0700

    Init

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+todoItems = []
+
+@app.route("/add/<item>")
+def addItem(item):
+    todoItems.append(item)
+    return "Added " + item + " to list!"
+
+@app.route("/items")
+def viewItems():
+    return ", ".join(todoItems)
+
+if __name__ == "__main__":
+    app.run(debug=True)
